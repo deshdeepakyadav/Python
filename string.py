0860759553fd8cb0462f17a08380b25dee793b0b
Desh Deepak Yadav <deshdeepak.yadav6@T-mobile.com>
2020-11-08 20:36:50 -0800

    python string

---
+++ string.py
@@ -0,0 +1,23 @@
+#String Literals
+#String literals in python are surrounded by either single quotation marks, or double quotation marks.
+#'hello' is the same as "hello".
+#You can display a string literal with the print() function:
+print("Hello")
+print('Hello')
+#Assign String to a Variable
+#Assigning a string to a variable is done with the variable name followed by an equal sign and the string:
+a = "Hello"
+print(a)
+#Multiline Strings
+#You can assign a multiline string to a variable by using three quotes:
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
+#Or three single quotes:
+a = '''Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua.'''
+print(a)
